6a0bfde7e627331d69e80b87ea717ca2b27ffab2
Lupita <lupitarivera8899@gmail.com>
2021-06-23 09:29:30 -0500

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  fetch('https://anapioficeandfire.com/api/books')
+  fetchedBooks = fetch('https://anapioficeandfire.com/api/books')
   .then(resp => resp.json())
   
   // .then(json => console.log(json))
@@ -8,9 +8,7 @@ function fetchBooks() {
   // .catch((err) => {
   //   console.log(err)
   // })
- 
-  
-  
+ return fetchedBooks;
 
 
 }
